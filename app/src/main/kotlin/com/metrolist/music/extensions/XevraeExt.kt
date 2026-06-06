881ead3d2f116c518b13839d25ee531c266aa6f2
Dare Dev <dev@dare.music>
2026-06-06 10:30:00 +0530

    fix: revert XevraeExt to kotlinx.datetime now that dependency is added

--- app/src/main/kotlin/com/metrolist/music/extensions/XevraeExt.kt
+++ app/src/main/kotlin/com/metrolist/music/extensions/XevraeExt.kt
@@ -6,21 +6,33 @@
 package com.metrolist.music.extensions
 
 import com.metrolist.music.db.entities.SearchHistory
-import java.time.LocalDateTime
-import java.time.ZoneId
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.minus
+import kotlinx.datetime.plus
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 
 fun List<SearchHistory>.toQueryList(): List<String> = map { it.query }
 
-fun now(): LocalDateTime = LocalDateTime.now()
+fun now(): LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
 
-fun LocalDateTime.isBefore2(other: LocalDateTime): Boolean = this.isBefore(other)
+fun LocalDateTime.isBefore2(other: LocalDateTime): Boolean = this < other
 
-fun LocalDateTime.isAfter2(other: LocalDateTime): Boolean = this.isAfter(other)
+fun LocalDateTime.isAfter2(other: LocalDateTime): Boolean = this > other
 
-fun LocalDateTime.plusSeconds(seconds: Long): LocalDateTime = this.plusSeconds(seconds)
+fun LocalDateTime.plusSeconds(seconds: Long): LocalDateTime =
+    this.toInstant(TimeZone.currentSystemDefault())
+        .plus(seconds, DateTimeUnit.SECOND, TimeZone.currentSystemDefault())
+        .toLocalDateTime(TimeZone.currentSystemDefault())
 
-fun startTimestampOfThisYear(): LocalDateTime = LocalDateTime.of(LocalDateTime.now().year, 1, 1, 0, 0, 0, 0)
+fun startTimestampOfThisYear(): LocalDateTime = LocalDateTime(now().year, 1, 1, 0, 0, 0, 0)
 
-fun LocalDateTime.beforeXDays(x: Int): LocalDateTime = this.minusDays(x.toLong())
+fun LocalDateTime.beforeXDays(x: Int): LocalDateTime =
+    this.date.minus(x, DateTimeUnit.DAY).atTime(this.time)
 
-fun LocalDateTime.toEpochMilli(): Long = this.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+fun LocalDateTime.toEpochMilli(): Long =
+    this.toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds()
